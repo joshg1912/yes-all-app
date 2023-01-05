18487c6c95d227cddffb3bd4af445897566ac154
unknown <joshgarcia7410@gmail.com>
2023-01-04 19:54:29 -0800

    adds controller methods

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -1,2 +1,33 @@
 class WorkoutsController < ApplicationController
+def index
+    workouts = Workout.all
+    render json: workouts.as_json
+end
+
+def create 
+    workout = Workout.new(
+        number:integer
+    )
+    workout.save
+    render json: workout.as_json
+end
+
+def show 
+    workout = Workout.find_by(id:params[:id])
+    render json: workout.as_json
+end
+
+def update
+    workout = Workout.find_by(id: params[:id])
+    workout.number = params[:name] || workout.name
+
+end
+
+def destroy
+    workout = Workout.find_by(id: params[:id])
+    workout.destroy
+    render json: {message: "workout deleted"}
+end
+
+
 end
